3459dcf574a7a7a6ad3c0979a03474c327d4e38b
Hemantkumar Gaikwad <hemantkumarg@synerzipune.local>
2018-04-17 18:52:33 +0530

    Nuxt js initial app setup

---
+++ components/Recipe.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-md-3" style="margin: 10px">
+    <nuxt-link :to="'Recepies/' + id">
+      <div class="card" style="width: 18rem; padding: 10px;">
+        <img
+          class="card-img-top"
+          :src="thumbnail"
+          alt="Card image cap"
+          width="100%"
+          height="300px"
+        >
+        <div class="card-body">
+          <p>{{previewText}}</p>
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['title', 'thumbnail', 'previewText', 'id']
+  }
+</script>
---
+++ pages/Recepies/_id/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <div class="row">
+      <div class="col-md-3" style="margin: 10px">
+        <div class="card" style="width: 18rem; padding: 10px;">
+          <h1>{{ $route.params.id}}</h1>
+          <div class="card-body">
+            <p>This is awesome food item.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
